53a272326275eef9a5ad5766b4d3a11bc82e65b3
Steffen Schneider <Steffen.Schneider@diva-e.com>
2025-12-22 12:50:37 +0100

    fix phph 8.2 vs 8.3 break

--- Classes/Constants/Constants.php
+++ Classes/Constants/Constants.php
@@ -4,20 +4,20 @@ namespace Hfm\Kursanmeldung\Constants;
 
 class Constants
 {
-    public const string HOTEL = 'hotel';
-    public const string HOTELS = 'hotels';
-    public const string GEBUEHREN = 'gebuehren';
-    public const string ORTE = 'orte';
-    public const string KURS ='kurs';
-    public const string KURSE ='kurse';
-    public const string DB_FIELD_KURS = 'kurs';
-    public const string ACTION_KURS_WAHL ='kurswahl';
-    public const string ACTION_STEP_1 ='step1';
-    public const string ACTION_STEP_1_DATA ='step1data';
-    public const string ACTION_STEP_2 ='step2';
-    public const string ACTION_STEP_2_DATA ='step2data';
-    public const string ACTION_STEP_3 ='step3';
-    public const string ACTION_STEP_3_DATA ='step3data';
-    public const string ACTION_STEP_4 ='step4';
-    public const string ACTION_STEP_4_DATA ='step4data';
+    public const HOTEL = 'hotel';
+    public const HOTELS = 'hotels';
+    public const GEBUEHREN = 'gebuehren';
+    public const ORTE = 'orte';
+    public const KURS ='kurs';
+    public const KURSE ='kurse';
+    public const DB_FIELD_KURS = 'kurs';
+    public const ACTION_KURS_WAHL ='kurswahl';
+    public const ACTION_STEP_1 ='step1';
+    public const ACTION_STEP_1_DATA ='step1data';
+    public const ACTION_STEP_2 ='step2';
+    public const ACTION_STEP_2_DATA ='step2data';
+    public const ACTION_STEP_3 ='step3';
+    public const ACTION_STEP_3_DATA ='step3data';
+    public const ACTION_STEP_4 ='step4';
+    public const ACTION_STEP_4_DATA ='step4data';
 }
